29d7bee86c2945d96af4b883a532958713f811a1
Kasp5953 <kasp5953@edu.easj.dk>
2018-03-05 11:18:46 +0100

    adds GetName() method

--- ClassLibraryCI/Class1.cs
+++ ClassLibraryCI/Class1.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibraryCI
-{
-    public class Class1
-    {
+namespace ClassLibraryCI {
+    public class Class1 {
+        public string GetName() {
+            return "name";
+        }
     }
 }
